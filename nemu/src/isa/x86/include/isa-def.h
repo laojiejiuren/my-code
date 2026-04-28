4ba03b03301c8d174da6e47dc5e11e4ad2975586
tracer-ysyx <tracer@ysyx.org>
2026-04-28 14:19:09 +0800

    > compile NEMU ysyx_22040000 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:19:09 up 18 min,  1 user,  load average: 0.36, 0.16, 0.06

--- nemu/src/isa/x86/include/isa-def.h
+++ nemu/src/isa/x86/include/isa-def.h
@@ -33,9 +33,9 @@ typedef struct {
       uint16_t _16;
       uint8_t  _8[2];   
     } gpr[8];            
-    struct {
+    //struct {
       uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-    };
+    //};
   };
   vaddr_t pc;            
 } x86_CPU_state;
