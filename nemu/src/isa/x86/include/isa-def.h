b7c27708b3e509b1584579799c36d5168d8b9bdc
lvzonghong <2473212358@qq.com>
2026-04-28 14:33:02 +0800

    通过X86寄存器测试

--- nemu/src/isa/x86/include/isa-def.h
+++ nemu/src/isa/x86/include/isa-def.h
@@ -27,13 +27,9 @@
  */
 
 typedef struct {
-  
-  union 
-  {
-    struct 
-    {
-      union 
-      {
+  union {
+    struct {
+      union {
         uint32_t _32;
         uint16_t _16;
         uint8_t  _8[2];
