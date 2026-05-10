0c17035bb103d1bad0b9c49c958bfe3711a5295e
lvzonghong <2473212358@qq.com>
2026-05-10 14:19:18 +0800

    判断时否是解引用

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -276,7 +276,8 @@ word_t expr(char *e, bool *success) {
 
 
   for (int i = 0; i < nr_token; i ++) 
-    if (tokens[i].type == '*' && (i == 0 || tokens[i - 1].type == TK_NUM) )
+    if (tokens[i].type == '*' && (i == 0 || tokens[i - 1].type == '(' || tokens[i - 1].type == '+'
+        tokens[i - 1].type == '-' || tokens[i - 1].type == '*' || tokens[i - 1].type == '/') )
       tokens[i].type = DEREF;
 
   eval_ok = true;
