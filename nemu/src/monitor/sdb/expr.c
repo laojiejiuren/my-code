c7f8dcb8aa6658712480204d20b168211f2b26f6
tracer-ysyx <tracer@ysyx.org>
2026-05-10 20:10:58 +0800

    > compile NEMU ysyx_22040000 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:10:58 up 48 min,  1 user,  load average: 0.32, 0.18, 0.06

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -47,7 +47,7 @@ static struct rule {
   {"[0-9]+u",TK_NUM},
   {"==", TK_EQ},        // equal
   {"0x[0-9A-Fa-f]+",HEX},
-  {"\\$[0-9A-Za-z]+",REGNAME},
+  {"\\$[$0-9A-Za-z]+",REGNAME},
   {"&&",AND},
   {"!=",TK_NEQ}
 };
