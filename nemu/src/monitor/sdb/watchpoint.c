9bf8a4a276434988230b5d83ca571df4b9d7dcb5
lvzonghong <2473212358@qq.com>
2026-05-14 19:31:15 +0800

    修改check_wp

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -135,8 +135,8 @@ bool check_wp()
     {
       flag = false;
       printf("ERROR WATCHPOINT:%d %s\n",p->NO,p->expr_str);
-      printf("Old val is %d\n",p->val);
-      printf("New val is %d\n",tmp);
+      printf("Old val is %u\n",p->val);
+      printf("New val is %u\n",tmp);
     }
     p = p->next;
   }
