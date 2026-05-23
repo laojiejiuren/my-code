51bbb2959968515021f681cf2e9989f928dea53b
tracer-ysyx <tracer@ysyx.org>
2026-05-23 14:53:09 +0800

    > compile NEMU ysyx_22040000 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:53:09 up  2:07,  1 user,  load average: 0.42, 0.23, 0.11

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -144,7 +144,7 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    uint32_t ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    uint32_t ret = system("gcc -Wall -Werror /tmp/.code.c -o /tmp/.expr");
     if (ret != 0) continue;
 
     fp = popen("/tmp/.expr", "r");
