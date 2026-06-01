e9e7247149e1e42759069de6dd40479c56ef141e
tracer-ysyx <tracer@ysyx.org>
2026-06-01 19:32:00 +0800

    > compile NEMU ysyx_22040000 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:32:00 up 15 min,  1 user,  load average: 0.04, 0.06, 0.05

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -147,7 +147,7 @@ static int cmd_p(char *args)
     printf("eval ERROR\n");
     return 0;
   }
-  printf("expr :%s  val: %u\n",args,val);
+  printf("expr :%s  val: %u %08x\n",args,val,val);
   return 0;
 }
 
