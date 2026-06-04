e252ebb3857d9b8d90aadbf20272b7c4a9c6919d
tracer-ysyx <tracer@ysyx.org>
2026-06-04 20:09:46 +0800

    > compile NEMU ysyx_22040000 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:09:46 up 4 min,  1 user,  load average: 0.19, 0.10, 0.03

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -47,7 +47,11 @@ $(OBJ_DIR)/%.i: %.c
 	@mkdir -p $(dir $@)
 	@$(CC) $(CFLAGS) -E -o $@ $<
 	$(call call_fixdep, $(@:.i=.d), $@)
+
+# 让 .o 依赖 .i，这样 make 才会触发 .i 的构建
+$(OBJ_DIR)/%.o: $(OBJ_DIR)/%.i
 endif
+
 
 # Depencies
 -include $(OBJS:.o=.d)
