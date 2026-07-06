52e75438e978aff58a341fd6b7a244b9a68a7e19
tracer-ysyx <tracer@ysyx.org>
2026-07-06 16:58:22 +0800

    > compile NEMU ysyx_22040000 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:58:22 up  1:52,  1 user,  load average: 0.03, 0.02, 0.01

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -138,6 +138,7 @@ int memcmp(const void *s1, const void *s2, size_t n) {
     d1++; d2++;
   }
   
+  if(n == 0) return 0;
   int val = (unsigned char)*d1 - (unsigned char)*d2;
 
   if (val == 0) return 0;
