783d51c374e85e8ab50f54c77a471d8fae55f1c3
lvzonghong <2473212358@qq.com>
2026-07-06 11:19:50 +0800

    添加memeset函数

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -92,7 +92,12 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  unsigned char * tmp = (unsigned char *)s;	
+	while(n--)
+	{
+		*tmp = (unsigned char)c;
+		tmp++;
+	}
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
