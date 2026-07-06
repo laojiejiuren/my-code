0988304688976ee08b887cfcc74346e53d3b63de
tracer-ysyx <tracer@ysyx.org>
2026-07-06 17:48:39 +0800

    > compile NEMU ysyx_22040000 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:48:39 up  2:47,  1 user,  load average: 0.06, 0.05, 0.00

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -115,9 +115,9 @@ static int decode_exec(Decode *s) {
 
   INSTPAT("0000001 ????? ????? 000 ????? 01100 11", mul    , R, R(rd) = src1 * src2);
   INSTPAT("0000001 ????? ????? 001 ????? 01100 11", mulh   , R, R(rd) = ((int64_t)(sword_t)src1 * (int64_t)(sword_t)src2) >> 32);
-  INSTPAT("0000001 ????? ????? 100 ????? 01100 11", div    , R, {if(src2 != 0)R(rd) = (sword_t)(src1) / (sword_t)src2; else R(rd) = 0xffffffff;if(src2 == -1) R(rd) = (sword_t)(src1);});
+  INSTPAT("0000001 ????? ????? 100 ????? 01100 11", div    , R, {if(src2 != 0)R(rd) = (sword_t)(src1) / (sword_t)src2; else R(rd) = 0xffffffff;if(src2 == -1) R(rd) = (sword_t)(src1);else R(rd) = (sword_t)(src1) / (sword_t)src2;});
   INSTPAT("0000001 ????? ????? 101 ????? 01100 11", divu   , R, {if(src2 != 0)R(rd) = src1 / src2; else R(rd) = 0xffffffff;});
-  INSTPAT("0000001 ????? ????? 110 ????? 01100 11", rem    , R, {if(src2 != 0)R(rd) = (sword_t)(src1) % (sword_t)src2; else R(rd) = (sword_t)(src1);if(src2 == -1) R(rd) = 0;});
+  INSTPAT("0000001 ????? ????? 110 ????? 01100 11", rem    , R, {if(src2 != 0)R(rd) = (sword_t)(src1) % (sword_t)src2; else R(rd) = (sword_t)(src1);if(src2 == -1) R(rd) = 0;else R(rd) = (sword_t)(src1) % (sword_t)src2;});
   INSTPAT("0000001 ????? ????? 111 ????? 01100 11", remu   , R, {if(src2 != 0)R(rd) = src1 % src2;else R(rd) = (src1);});
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
