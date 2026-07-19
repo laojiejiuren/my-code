5e3fd4c1682d274a5967702ba3cb4563e2a1eb90
tracer-ysyx <tracer@ysyx.org>
2026-07-19 20:06:39 +0800

    > compile NEMU ysyx_22040000 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:06:39 up 28 min,  1 user,  load average: 0.18, 0.16, 0.10

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -40,7 +40,7 @@ static void check_bound(IOMap *map, paddr_t addr) {
     Assert(addr <= map->high && addr >= map->low,
         "address (" FMT_PADDR ") is out of bound {%s} [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
         addr, map->name, map->low, map->high, cpu.pc);
-    printf("%s\n",map->name);
+    printf("IOE is :%s\n",map->name);
   }
 }
 
