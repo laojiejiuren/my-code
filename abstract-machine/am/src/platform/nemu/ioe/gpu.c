f63a1d77f400d4d019d4e6661481a46ce5f4e087
tracer-ysyx <tracer@ysyx.org>
2026-07-24 19:22:07 +0800

    > compile NEMU ysyx_22040000 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:22:07 up  4:57,  1 user,  load average: 0.09, 0.08, 0.02

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,14 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+/*
   int i;
   int w = 400;  // TODO: get the correct width
   int h = 300;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 5);
+  outl(SYNC_ADDR, 1);
+  */
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -21,8 +23,19 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = (uint32_t *)ctl->pixels;
+  int w = ctl->w;
+  int h = ctl->h;
+  int y = ctl->y;
+
+  for(int i = 0;i < h; ++i) //枚举的是图像内部的行
+    for(int j = 0;j < w; ++j)
+      fb[(i + y) * 400 + j] = pixels[i * w + j];
+
+
   if (ctl->sync) {
-    outl(SYNC_ADDR, 6);
+    outl(SYNC_ADDR, 1);
   }
 }
 
