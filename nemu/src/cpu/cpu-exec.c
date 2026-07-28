22fc7e2e115d63c6c34774d0f98b5bba4cc68f1f
tracer-ysyx <tracer@ysyx.org>
2026-07-28 15:49:37 +0800

    > compile NEMU ysyx_22040000 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:49:37 up  1:39,  1 user,  load average: 0.12, 0.08, 0.02

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -46,7 +46,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   {
     for(int i = 0; i < 20; ++i)
     {
-      if(i == RING_N - 1)
+      if(i == RING_N - 1)//默认加一后会有偏移
       {
         printf("--> ");
         puts(_this->ringbuf[i]);
