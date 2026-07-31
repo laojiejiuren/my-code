fe701307181c9a0aa19ee6c6e367cd9a86a3a578
tracer-ysyx <tracer@ysyx.org>
2026-07-31 11:48:35 +0800

    > compile NEMU ysyx_22040000 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:48:35 up  2:25,  1 user,  load average: 0.13, 0.08, 0.02

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -82,8 +82,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
       if(MTRACE_COND)
       {  
         snprintf(wmembuf,sizeof(wmembuf),"pc: "FMT_WORD" addr: "FMT_PADDR" len: %d data: "FMT_WORD"",cpu.pc,addr,len,data);
-        int tmp = MTRACE_COND;
-        printf("%d",tmp);
+        puts(MTRACE_COND);
       }
     #endif
     pmem_write(addr, len, data); 
