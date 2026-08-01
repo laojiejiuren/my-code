ba45b905fc42a6368e59c5239ff56d0003350559
tracer-ysyx <tracer@ysyx.org>
2026-08-01 21:17:04 +0800

    > compile NEMU ysyx_22040000 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:17:04 up 12:43,  1 user,  load average: 0.05, 0.09, 0.08

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -89,8 +89,8 @@ void init_ftrace(const char *elf_file)
 
     for(int i = 0; i < sym_num; ++i)
     {
-        //char * tmp = strtab + symtab[i].st_name;
-        printf("ffff");
+        char * tmp = strtab + symtab[i].st_name;
+        printf("%s",tmp);
     }
 
     fclose(F);
