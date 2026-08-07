aee64cb973d675ee8a9baa0546be01c350985f99
tracer-ysyx <tracer@ysyx.org>
2026-08-07 14:27:44 +0800

    > compile NEMU ysyx_26050158 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:27:44 up  5:03,  1 user,  load average: 0.10, 0.09, 0.06

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -16,7 +16,7 @@ int add_buff(char *out, const char *fmt, va_list ap)
       tmp++;
       if(*tmp == 'd')
       {
-        long num = va_arg(ap, long);
+        long num = va_arg(ap, int);
         char buf[20] = {};
         int id = 0;
 
