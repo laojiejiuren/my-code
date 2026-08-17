8f415fbdafe20b449ab16fb30a0d6076db9134e2
tracer-ysyx <tracer@ysyx.org>
2026-08-17 19:29:44 +0800

    > compile NEMU ysyx_26050158 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:29:44 up 10:48,  1 user,  load average: 0.46, 0.16, 0.05

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -32,7 +32,6 @@ void init_log(const char *log_file) {
 
 bool log_enable() {
   return MUXDEF(CONFIG_TRACE, (g_nr_guest_inst >= CONFIG_TRACE_START) &&
-         (g_nr_guest_inst <= 12000000), false);
+         (g_nr_guest_inst <= CONFIG_TRACE_END), false);
 }
 #endif
-//CONFIG_TRACE_END
