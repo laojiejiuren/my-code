4ea7eb2162c46e9c11c24598da20d4dec84f1e43
tracer-ysyx <tracer@ysyx.org>
2026-08-18 16:14:00 +0800

    > compile NEMU ysyx_26050158 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:14:00 up  7:14,  1 user,  load average: 0.16, 0.19, 0.23

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -31,7 +31,8 @@ void etrace_printf(uint32_t epc, uint32_t mtvec, uint32_t mcause, bool flag)
   etrace_write("etrace: %s epc = "FMT_WORD" mtvec = "FMT_WORD" mcause = "FMT_WORD" a7 = "FMT_WORD" \n",
                ch, epc, mtvec, mcause, R(17));
 }
-
+#else
+void etrace_printf(uint32_t epc, uint32_t mtvec, uint32_t mcause, bool flag){}
 #endif
 
 
@@ -143,11 +144,11 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000001 ????? ????? 111 ????? 01100 11", remu   , R, {if(src2 != 0)R(rd) = src1 % src2;else R(rd) = (src1);});
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(8, s->pc); if(CONFIG_ETRACE) etrace_printf(s->pc, cpu_csr.mtvec, 8, 1));
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(8, s->pc); etrace_printf(s->pc, cpu_csr.mtvec, 8, 1));
   INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , C, { R(rd) = csr_read(imm); csr_write(imm, src1); });
   INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , C, { word_t tmp = csr_read(imm); if (src1 != 0) csr_write(imm, tmp | src1); R(rd) = tmp; });
   INSTPAT("??????? ????? ????? 011 ????? 11100 11", csrrc  , C, { word_t tmp = csr_read(imm); if (src1 != 0) csr_write(imm, tmp & ~src1); R(rd) = tmp; });
-  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc = cpu_csr.mepc; if(CONFIG_ETRACE) etrace_printf(s->pc, cpu_csr.mtvec, cpu_csr.mcause, 0));
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc = cpu_csr.mepc; etrace_printf(s->pc, cpu_csr.mtvec, cpu_csr.mcause, 0));
 
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
