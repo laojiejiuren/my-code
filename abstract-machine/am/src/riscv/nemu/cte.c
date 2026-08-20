76749cbedcb23b9ad0ad6db3eb2ae36835676ffb
tracer-ysyx <tracer@ysyx.org>
2026-08-20 20:21:06 +0800

    > compile NEMU ysyx_26050158 lvzonghong Linux LAPTOP-F9JEFTU3 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:21:06 up 11:35,  1 user,  load average: 0.04, 0.08, 0.08

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,6 +9,7 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       case ENVIRONMENT_CALL_FROM_U_MODE: 
+      case ENVIRONMENT_CALL_FROM_M_MODE:
       ev.event = EVENT_YIELD; c->mepc += 4; break;
       default: ev.event = EVENT_ERROR; break;
     }
